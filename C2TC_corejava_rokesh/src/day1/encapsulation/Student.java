9bb6cf7136cd6159223cdb94391ceac57455922d
ETHNOTECH <ETHNOTECH@DESKTOP-SE2565O>
2025-07-23 21:11:36 -0700

    Merge branch 'master' of https://github.com/Rokesh252005/C2TC_corejava.git

--- C2TC_corejava_rokesh/src/day1/encapsulation/Student.java
+++ C2TC_corejava_rokesh/src/day1/encapsulation/Student.java
@@ -1,10 +1,22 @@
-package day1.encapsulation;
+//Entity Class
+package Day1.Encapsulation;
 
 public class Student {
-	public static void main(String[]args) {
-		Student ob = new Student();
-		ob.id=101;
-		System.out.println(ob.id)
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
 	}
-
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
 }
